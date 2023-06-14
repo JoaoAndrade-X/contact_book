249809bb7b74fbe18507efd8482b7d9fbb9e59ed
OVerdadeiroX <xtudo.vestibulares@gmail.com>
2023-06-14 21:58:12 +0100

    Creating GET and SET for Contact

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -10,7 +10,6 @@ final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
 class ContactHelper {
-
   static final ContactHelper _instance = ContactHelper.internal();
 
   factory ContactHelper() => _instance;
@@ -20,7 +19,7 @@ class ContactHelper {
   Database _db;
 
   Future<Database> get db async {
-    if(_db != null) {
+    if (_db != null) {
       return _db;
     } else {
       _db = await initDb();
@@ -32,13 +31,33 @@ class ContactHelper {
     final databasePath = await getDatabasesPath();
     final path = join(databasePath, "contacts.db");
 
-    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async {
+    return await openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
       await db.execute(
-        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)"
-      );
+          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)");
     });
   }
 
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  Future<Contact> getContact(int id) async {
+    Database dbContact = await db;
+    List<Map> maps = await dbContact.query(
+      contactTable,
+      columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
+      where: "$idColumn = ?",
+      whereArgs: [id]
+    );
+    if(maps.length > 0) {
+      return Contact.fromMap(maps.first);
+    } else {
+      return null;
+    }
+  }
 }
 
 class Contact {
@@ -61,9 +80,9 @@ class Contact {
       nameColumn: name,
       emailColumn: email,
       phoneColumn: phone,
-      imgColumn:img
+      imgColumn: img
     };
-    if(id != null) {
+    if (id != null) {
       map[idColumn] = id;
     }
     return map;
